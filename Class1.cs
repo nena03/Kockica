bab03ef46d8787ad9439bcdbdc52a3aae3d69546
nena03 <69650515+nena03@users.noreply.github.com>
2020-12-03 16:28:26 +0100

    Kockica

---
+++ Class1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    using System.Drawing;
+
+    class Tacka
+    {
+        private int x, y;
+        private Color boja;
+
+        public Tacka(int x, int y)
+        {
+            if (x >= 0 && y >= 0)
+            {
+                this.x = x;
+                this.y = y;
+            }
+            else
+                throw new Exception("Koordinate su <0!");
+        }
+
+        public void PostaviBoju(Color boja)
+        {
+            this.boja = boja;
+        }
+
+        public void Crtaj(Graphics g)
+        {
+            SolidBrush cetka = new SolidBrush(boja);
+            g.FillEllipse(cetka, x - 3, y - 3, 6, 6);
+        }
+    }
+
+
+    class Kockica
+    {
+        private int x, y, a = 100, n;
+        private Color boja = Color.Black;
+
+        public Kockica(int x, int y, int a, int n)
+        {
+            if (x >= 50 && y >= 50)
+            {
+                this.x = x;
+                this.y = y;
+                this.n = n;
+            }
+            else
+                throw new Exception("Kockica bi izasla!");
+        }
+
+        public void Crtaj(Graphics g, int n, int x, int y, int a)
+        {
+            Point t1 = new Point(x - a / 2, y - a / 2);
+            Point t2 = new Point(x + a / 2, y - a / 2);
+            Point t3 = new Point(x + a / 2, y + a / 2);
+            Point t4 = new Point(x - a / 2, y + a / 2);
+
+            Pen olovka = new Pen(Color.Black);
+            Point[] temena = { t1, t2, t3, t4 };
+            g.DrawPolygon(olovka, temena);
+
+            if (n % 2 == 1)
+            {
+                Tacka p1 = new Tacka(x, y);
+                p1.PostaviBoju(Color.Black);
+                p1.Crtaj(g);
+            }
+            if (n > 1)
+            {
+                Tacka p2 = new Tacka(x - a / 4, y - a / 4);
+                p2.PostaviBoju(Color.Black);
+                p2.Crtaj(g);
+                Tacka p3 = new Tacka(x + a / 4, y + a / 4);
+                p3.PostaviBoju(Color.Black);
+                p3.Crtaj(g);
+            }
+            if (n >= 4)
+            {
+                Tacka p4 = new Tacka(x + a / 4, y - a / 4);
+                p4.PostaviBoju(Color.Black);
+                p4.Crtaj(g);
+                Tacka p5 = new Tacka(x - a / 4, y + a / 4);
+                p5.PostaviBoju(Color.Black);
+                p5.Crtaj(g);
+            }
+            if (n == 6)
+            {
+                Tacka p6 = new Tacka(x - a / 4, y);
+                p6.PostaviBoju(Color.Black);
+                p6.Crtaj(g);
+                Tacka p7 = new Tacka(x + a / 4, y);
+                p7.PostaviBoju(Color.Black);
+                p7.Crtaj(g);
+            }
+        }
+    }
+
+
+}
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        
+            int x, y, m1, m2, c1, c2, v1, v2, p1, p2, l1, l2, o1, o2;
+
+            private void Form1_Load(object sender, EventArgs e)
+            {
+               
+                m1 = ClientRectangle.Width / 3;
+                m2 = ClientRectangle.Height / 3;
+                c1 = ClientRectangle.Width / 2;
+                c2=ClientRectangle.Height / 3;
+                v1 = ClientRectangle.Width * 2 / 3;
+                v2 = ClientRectangle.Height / 3;
+            p1 = ClientRectangle.Width / 3;
+            p2 = ClientRectangle.Height * 2 / 3;
+            l1 = ClientRectangle.Width / 2;
+            l2 = ClientRectangle.Height * 2 / 3;
+            o1 = ClientRectangle.Width * 2 / 3;
+            o2 = ClientRectangle.Height * 2 / 3;
+
+
+        }
+
+            Random n = new Random();
+
+            private void button1_Click(object sender, EventArgs e)
+            
+            {
+                Refresh();
+                Graphics g = CreateGraphics();
+               
+                Kockica m = new Kockica(m1, m2, 100, n.Next(6) + 1);
+                Kockica c= new Kockica(c1, c2, 100, n.Next(6) + 1);
+                Kockica v= new Kockica(v1, v2, 100, n.Next(6) + 1);
+                Kockica p= new Kockica(p1, p2, 100, n.Next(6) + 1);
+                Kockica l= new Kockica(l1, l2, 100, n.Next(6) + 1);
+                Kockica o = new Kockica(o1, o2, 100, n.Next(6) + 1);
+
+
+            
+                m.Crtaj(g, n.Next(6) + 1, m1, m2, 100);
+                c.Crtaj(g, n.Next(6) + 1, c1, c2, 100);
+                v.Crtaj(g, n.Next(6) + 1, v1, v2, 100);
+                p.Crtaj(g, n.Next(6) + 1, p1, p2, 100);
+                l.Crtaj(g, n.Next(6) + 1, l1, l2, 100);
+                o.Crtaj(g, n.Next(6) + 1, o1, o2, 100);
+
+
+
+        }
+
+
+        
+    }
+}
